ce824871a43616126f33bc8268f5a1361d5762bd
MogaRaluca <ralucamoga12@gmail.com>
2022-10-18 17:17:00 +0300

    Add project files.

---
+++ Data/Moga_Raluca_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moga_Raluca_Lab2.Models;
+
+namespace Moga_Raluca_Lab2.Data
+{
+    public class Moga_Raluca_Lab2Context : DbContext
+    {
+        public Moga_Raluca_Lab2Context (DbContextOptions<Moga_Raluca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moga_Raluca_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Moga_Raluca_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
