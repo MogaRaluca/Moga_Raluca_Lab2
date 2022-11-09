9d8556007118eca7cca11e62db1061c3f8dde9a7
MogaRaluca <ralucamoga12@gmail.com>
2022-11-09 17:11:41 +0200

    Lab5

--- Data/Moga_Raluca_Lab2Context.cs
+++ Data/Moga_Raluca_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Moga_Raluca_Lab2.Data
         public DbSet<Moga_Raluca_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Moga_Raluca_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Moga_Raluca_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Moga_Raluca_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
--- Migrations/20221108142904_Borrowings.Designer.cs
+++ Migrations/20221108142904_Borrowings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Moga_Raluca_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Moga_Raluca_Lab2.Data;
 namespace Moga_Raluca_Lab2.Migrations
 {
     [DbContext(typeof(Moga_Raluca_Lab2Context))]
-    partial class Moga_Raluca_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221108142904_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
